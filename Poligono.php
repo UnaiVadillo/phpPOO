17ffc57be910fdae5e79c0784baf306804bc2ef2
UnaiVadillo <56158238+UnaiVadillo@users.noreply.github.com>
2020-12-09 10:46:57 +0100

    Add files via upload

---
+++ Poligono.php
@@ -0,0 +1,48 @@
+<?php
+
+
+abstract class Poligono
+{
+    private $color, $altura, $anchura;
+
+    public function __construct($color, $altura, $anchura)
+    {
+        $this->color = $color;
+        $this->altura = $altura;
+        $this->anchura = $anchura;
+    }
+
+    public function getColor()
+    {
+        return $this->color;
+    }
+
+    public function setColor($color)
+    {
+        $this->color = $color;
+    }
+
+    public function getAltura()
+    {
+        return $this->altura;
+    }
+
+    public function setAltura($altura)
+    {
+        $this->altura = $altura;
+    }
+
+    public function getAnchura()
+    {
+        return $this->anchura;
+    }
+
+    public function setAnchura($anchura)
+    {
+        $this->anchura = $anchura;
+    }
+
+    public function area(){
+
+    }
+}
