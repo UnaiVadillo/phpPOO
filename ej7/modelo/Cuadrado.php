b044634b1975bda447ebce78b738a51388b92ef1
UnaiVadillo <56158238+UnaiVadillo@users.noreply.github.com>
2020-12-16 10:40:56 +0100

    Add files via upload

--- ej7/modelo/Cuadrado.php
+++ ej7/modelo/Cuadrado.php
@@ -5,11 +5,11 @@ require_once "Poligono.php";
 class Cuadrado extends Poligono
 {
     function area(){
-        if($this->anchura < 0 || $this->anchura < 0) {
+        if($this->getAltura() < 0 || $this->getAnchura() < 0) {
             // Al estar dentro de un namespace, que utilizar \Exception para referirnos al root namespace
             // Si no intentaría buscar la clase Exception en en namespace actual.
             throw new \Exception("Uno de los lados es negativo.");
         }
-        return $this->anchura * $this->altura;
+        return $this->getAnchura() * $this->getAltura();
     }
 }
